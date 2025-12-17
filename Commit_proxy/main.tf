93dd59c2669159ccf29f2acbe55a7e7012936757
Nishwanth-eq <nishwanth@equicomtech.com>
2025-12-17 17:13:11 +0530

    Update main.tf

--- Commit_proxy/main.tf
+++ Commit_proxy/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "aws_instance" "this" {
-  count                = 3
+  count                = var.instance_count
   ami                  = data.aws_ami.ubuntu.id
   instance_type        = "c7g.large"
   iam_instance_profile = aws_iam_instance_profile.fdb_instance_profile.name
