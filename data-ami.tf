339a1f4fbc80e138a340bc198dc342b93a316f59
Bonagiri Sai Nishwanth <nishwanth@equicomtech.com>
2025-12-17 16:50:35 +0530

    remove all files

--- data-ami.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-24.04-noble-server-*"]
-  }
-}
