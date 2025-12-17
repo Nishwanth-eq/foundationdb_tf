e25357f6a5d6f47c1fdce39826b5714f169800d1
Nishwanth-eq <nishwanth@equicomtech.com>
2025-12-17 17:09:41 +0530

    Fix: Replace 'var.count' with 'var.instance_count' in Coordinators/main.tf

--- Coordinators/main.tf
+++ Coordinators/main.tf
@@ -1,7 +1,7 @@
 locals { role_class = "stateless" }
 
 resource "aws_instance" "this" {
-  count         = var.count
+  count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
 
